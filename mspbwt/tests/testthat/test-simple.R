20b90731e6441239cada59049ca3fbdb0a43f473
rwdavies <robertwilliamdavies@gmail.com>
2022-04-28 16:10:03 +0100

    remove useless test

--- mspbwt/tests/testthat/test-simple.R
+++
@@ -1,9 +0,0 @@
-
-test_that("can properly determine integer is odd using R", {
-    expect_equal(R_is_odd(10L), FALSE)
-    expect_equal(R_is_odd(11L), TRUE)
-})
-test_that("can properly determine integer is odd using Rcpp", {
-    expect_equal(cpp_is_odd(10L), FALSE)
-    expect_equal(cpp_is_odd(11L), TRUE)
-})
