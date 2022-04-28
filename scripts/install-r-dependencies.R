39e11a5c63210e9f54b3ad1e2596f04c8840a275
rwdavies <robertwilliamdavies@gmail.com>
2022-04-28 16:06:07 +0100

    Try adding workflow to repo

---
+++ scripts/install-r-dependencies.R
@@ -0,0 +1,18 @@
+#!/usr/bin/env Rscript
+
+required_packages <- c("Rcpp", "data.table")
+for(package in required_packages) {
+    if (!suppressPackageStartupMessages(require(package, character.only = TRUE))) {
+        out <- install.packages(package, repos="http://cran.rstudio.com/")
+        out <- require(package, character.only = TRUE)
+        if (!out) {
+            stop(paste0("Failed to install package:", package))
+        }
+    }
+}
+if (!suppressPackageStartupMessages(require("rrbgen")))
+    install.packages("https://github.com/rwdavies/rrbgen/releases/download/0.0.6/rrbgen_0.0.6.tar.gz", repos=NULL)
+
+if (!suppressPackageStartupMessages(require("STITCH")))
+    install.packages("https://github.com/rwdavies/STITCH/releases/download/1.6.6/STITCH_1.6.6.tar.gz", repos=NULL)
+
