2a8e05ddf414a85635acdd76a5aa23426ca0d753
Zilong-Li <zimusen94@gmail.com>
2024-08-04 20:24:56 +0200

    update scripts

--- scripts/install-r-dependencies.R
+++ scripts/install-r-dependencies.R
@@ -14,5 +14,5 @@ if (!suppressPackageStartupMessages(require("rrbgen")))
     install.packages("https://github.com/rwdavies/rrbgen/releases/download/0.0.6/rrbgen_0.0.6.tar.gz", repos=NULL)
 
 if (!suppressPackageStartupMessages(require("STITCH")))
-    install.packages("https://github.com/rwdavies/STITCH/releases/download/1.6.11/STITCH_1.6.11.tar.gz", repos=NULL)
+    install.packages("https://github.com/rwdavies/STITCH/releases/download/1.7.0/STITCH_1.7.0.tar.gz", repos=NULL)
 
